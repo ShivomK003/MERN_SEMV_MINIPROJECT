733bf92786e76e48ea31482c2e56f2f13f08bbfa
ShivomK003 <shivomkarnad@gmail.com>
2023-11-13 21:01:35 +0530

    backend set

--- api/index.js
+++ api/index.js
@@ -20,7 +20,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: 'http://65523dc1baa62402e69e4aad--admirable-praline-0ad70a.netlify.app',
+}));
 
 app.get('/test', (req, res) => {
   res.json('test ok')
